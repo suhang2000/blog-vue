42f892b6b9a03bd1d1f27715bb7ea0bcd9bda150
bcloak <wysbcloak@gmail.com>
2021-05-27 15:01:54 +0800

    修改添加用户

--- src/components/admin/AddUser.vue
+++ src/components/admin/AddUser.vue
@@ -7,42 +7,42 @@
       <el-avatar :size="50" icon="el-icon-user-solid"></el-avatar>
     </div>
     <el-form-item prop="username">
-      <el-input type="text" v-model="regisForm.username"
-                auto-complete="off" placeholder="登录昵称"></el-input>
+      <el-input type="text" v-model="regisForm.username" maxlength="16"
+                auto-complete="off" placeholder="用户名(小于16个字符)"></el-input>
     </el-form-item>
-    <el-form-item prop="password">
-      <el-input type="password" v-model="regisForm.password"
-                auto-complete="off" placeholder="登录密码"></el-input>
+    <el-form-item prop="user_password">
+      <el-input type="password" v-model="regisForm.user_password" maxlength="16"
+                auto-complete="off" placeholder="登录密码(小于16个字符)"></el-input>
     </el-form-item>
     <el-form-item prop="verifypwd">
-      <el-input type="password" v-model="regisForm.verifypwd"
+      <el-input type="password" v-model="regisForm.verifypwd" maxlength="16"
                 auto-complete="off" placeholder="再次输入登录密码"></el-input>
     </el-form-item>
-    <el-form-item prop="phone">
-      <el-input type="text" v-model="regisForm.phone"
-                auto-complete="off" placeholder="电话号码"></el-input>
+      <el-form-item prop="email">
+      <el-input type="text" v-model="regisForm.email" maxlength="40"
+                auto-complete="off" placeholder="邮箱"></el-input>
+    </el-form-item>
+    <el-form-item prop="phone_number">
+      <el-input type="text" v-model="regisForm.phone_number"
+                auto-complete="off" placeholder="电话号码（选填）"></el-input>
     </el-form-item>
     <el-radio v-model="regisForm.gender" label="F">男</el-radio>
     <el-radio v-model="regisForm.gender" label="M">女</el-radio>
     <el-form-item style="width: 100%">
-      <el-button type="primary" style="width: 40%;background: #505458;border: none"
-                 @click="validate_register('regisForm')">添加
-      </el-button>
+      <el-button type="primary" style="width: 40%;background: #505458;border: none" @click="validate_register('regisForm')">注册</el-button>
     </el-form-item>
   </el-form>
   </body>
 </template>
-
 <script>
-import {validatePhone} from '../../utils/validate'
+import {validatePhone, validateEmail} from '../../utils/validate'
 
-export default {
-  name: 'AddUser',
+export default{
   data () {
     const validatePassword = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('再次输入登陆密码！'))
-      } else if (value !== this.regisForm.password) {
+      } else if (value !== this.regisForm.user_password) {
         callback(new Error('两次密码必须相同！'))
       } else {
         callback()
@@ -51,38 +51,48 @@ export default {
     return {
       rules: {
         username: [{required: true, message: '姓名不能为空', trigger: 'blur'}],
-        password: [{required: true, message: '密码不能为空', trigger: 'blur'}],
+        user_password: [{required: true, message: '密码不能为空', trigger: 'blur'}],
         verifypwd: [{required: true, validator: validatePassword, trigger: 'blur'}],
-        phone: [{required: true, validator: validatePhone, trigger: 'blur'}]
+        phone_number: [{required: false, validator: validatePhone, trigger: 'blur'}],
+        email: [{required: true, validator: validateEmail, trigger: 'blur'}]
       },
       regisForm: {
         username: '',
-        password: '',
+        user_password: '',
         verifypwd: '',
-        phone: '',
+        phone_number: '',
+        email: '',
+        profile_photo: '',
         gender: ''
       }
     }
   },
   methods: {
     register () {
-      this.$axios.post('/register/user', {
-        username: this.regisForm.username,
-        user_password: this.regisForm.password,
-        phone_number: this.regisForm.phone,
-        gender: this.regisForm.gender
-      }).then(resp => {
-        if (resp.data.code === 200) {
-          this.$alert('添加成功', '提示', {
-            confirmButtonText: '确定'
-          })
-        } else {
-          this.$alert(resp.data.message, '提示', {
-            confirmButtonText: '确定'
-          })
-        }
-      }).catch(failResponse => {
-      })
+      var _this = this
+      this.$axios
+        .post('/register/user', {
+          username: this.regisForm.username,
+          user_password: this.regisForm.user_password,
+          phone_number: this.regisForm.phone,
+          email: this.regisForm.email,
+          profile_photo: this.regisForm.profile_photo,
+          gender: this.regisForm.gender
+        })
+        .then(resp => {
+          if (resp.data.code === 200) {
+            this.$alert('注册成功', '提示', {
+              confirmButtonText: '确定'
+            })
+            var path = _this.$route.query.redirect
+            _this.$router.replace({path: path === '/' || path === undefined ? '/login' : path})
+          } else {
+            this.$alert(resp.data.message, '提示', {
+              confirmButtonText: '确定'
+            })
+          }
+        })
+        .catch(failResponse => {})
     },
     validate_register (formName) {
       this.$refs[formName].validate((valid) => {
@@ -97,8 +107,7 @@ export default {
   }
 }
 </script>
-
-<style scoped>
+<style>
   .regis-container {
     border-radius: 15px;
     background-clip: padding-box;
@@ -109,13 +118,11 @@ export default {
     border: 1px solid #eaeaea;
     box-shadow: 0 0 25px #cac6c6;
   }
-
   .regis_title {
     margin: 0 auto 40px auto;
     text-align: center;
     color: #505458;
   }
-
   #paper {
     /*background: url("../../assets/regisBG.jpg") no-repeat center;*/
     height: 100%;
@@ -123,5 +130,7 @@ export default {
     background-size: cover;
     position: fixed;
   }
-
+  body{
+    margin: -5px 0;
+  }
 </style>
